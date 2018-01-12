ee4ecdbe74fad1c857d6f8c8552060d40915e59c
dev-keenability <dev@keenability.com>
2018-01-12 13:48:00 -0500

    Pass data from parent component to child component, and pass parent function to child component

--- src/app/index.js
+++ src/app/index.js
@@ -2,27 +2,77 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var CreateReactClass = require('create-react-class');
 
+
+
 var Product = CreateReactClass({
   getInitialState: function() {
     return {qty: 0};
   },
 
   buy: function() {
-    this.setState()
+    this.setState({qty: this.state.qty + 1});
+  },
+
+  show: function(){
+    this.props.handleShow(this.props.name)
   },
 
   render: function(){
     return(
       <div>
-        <p>Android - $199</p>
+        <p>{this.props.name} - ${this.props.price}</p>
         <button onClick={this.buy}>Buy</button>
+        <button onClick={this.show}>Show</button>
+        <h3>Qty: {this.state.qty} item(s)</h3>
+        <hr/>
       </div>
     );
   }
 });
 
 
-ReactDOM.render(<Product />, document.getElementById('root'));
+
+
+var Total = CreateReactClass({
+  render: function() {
+    return(
+      <div>
+        <h3>Total Cash: </h3>
+      </div>
+    );
+  }
+});
+
+
+
+
+var ProductList = CreateReactClass({
+  showProduct: function(name) {
+    alert("You selected " + name);
+  },
+
+  render: function() {
+    return(
+      <div>
+        <Product name="Android" price="121" handleShow={this.showProduct} />
+        <Product name="Apple" price="123" handleShow={this.showProduct} />
+        <Product name="Nokia" price="65" handleShow={this.showProduct} />
+        <Total />
+      </div>
+    );
+  }
+});
+
+
+
+
+
+
+
+
+
+
+ReactDOM.render(<ProductList />, document.getElementById('root'));
 
 
 
